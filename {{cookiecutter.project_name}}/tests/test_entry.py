ff4d365ab5c508e5d1513a9a83833fc273b1f2fe
Vaughn Kottler <vaughnkottler@gmail.com>
2021-10-23 00:16:55 -0700

    Close #1 - Fix unit-test coverage

--- {{cookiecutter.project_name}}/tests/test_entry.py
+++ {{cookiecutter.project_name}}/tests/test_entry.py
@@ -2,6 +2,9 @@
 {{cookiecutter.project_slug}} - Test the program's entry-point.
 """
 
+# built-in
+from unittest.mock import patch
+
 # module under test
 from {{cookiecutter.project_slug}} import PKG_NAME
 from {{cookiecutter.project_slug}}.entry import main as {{cookiecutter.project_slug}}_main
@@ -12,3 +15,6 @@ def test_entry_basic():
 
     args = [PKG_NAME]
     assert {{cookiecutter.project_slug}}_main(args) == 0
+
+    with patch("{{cookiecutter.project_slug}}.entry.entry", side_effect=SystemExit(1)):
+        assert {{cookiecutter.project_slug}}_main(args) != 0
