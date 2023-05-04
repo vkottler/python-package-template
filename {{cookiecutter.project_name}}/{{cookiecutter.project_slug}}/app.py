c54f700362166531c5bff268cf3c0ecf409fe1ff
Vaughn Kottler <vaughnkottler@gmail.com>
2023-05-04 00:31:33 -0500

    Add support for sub-commands

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/app.py
@@ -1,17 +1,31 @@
 """
-{{cookiecutter.project_slug}} - This package's command-line entry-point application.
+This package's command-line entry-point application.
 """
 
 # built-in
-import argparse
+from argparse import ArgumentParser as _ArgumentParser
+from argparse import Namespace as _Namespace
+from typing import Optional as _Optional
+
+# third-party
+from vcorelib.args import CommandFunction as _CommandFunction
+from vcorelib.args import app_args as _app_args
+
+# internal
+from {{cookiecutter.project_slug}}.commands.all import commands
+
+COMMAND: _Optional[_CommandFunction] = None
 
 
-def entry(args: argparse.Namespace) -> int:
+def entry(args: _Namespace) -> int:
     """Execute the requested task."""
 
-    print(args)
-    return 0
+    assert COMMAND is not None
+    return COMMAND(args)
 
 
-def add_app_args(_: argparse.ArgumentParser) -> None:
+def add_app_args(parser: _ArgumentParser) -> None:
     """Add application-specific arguments to the command-line parser."""
+    global COMMAND  # pylint: disable=global-statement
+    add, COMMAND = _app_args(commands, {})
+    add(parser)
